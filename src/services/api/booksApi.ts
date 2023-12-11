d45dfbcba85bc1d6bb706cc433655760d8574d95
lucasar2303 <lucas-ar13@hotmail.com>
2023-12-11 09:57:25 -0300

    fix1

--- src/services/api/booksApi.ts
+++ src/services/api/booksApi.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const API_KEY = import.meta.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+const API_KEY = import.meta.env.VITE_REACT_APP_GOOGLE_BOOKS_API_KEY;
 const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
+
 
 // Função para buscar livros
 export const searchBooks = async (query: string) => {
   try {
+    console.log(API_KEY)
     const response = await axios.get(`${BASE_URL}?q=${query}&key=${API_KEY}`);
     console.log(response.data);  // Imprime a resposta no console
     return response.data;
